d62d134c9e67e573a9854f2d5c4663690fafad63
abhishekvispute <abhivispute33@gmail.com>
2022-12-03 01:53:36 +0530

    feat: totalSupply

--- contracts/NFTSale.sol
+++ contracts/NFTSale.sol
@@ -12,7 +12,8 @@ import {IConnext} from "@connext/nxtp-contracts/contracts/core/connext/interface
 contract ETHIndia22NFTSale is ERC721 {
 
     uint256 public constant mintPrice = 1e16; // 0.01 ETH
-    
+    uint256 public constant totalSupply = 100;
+
     uint32 counter = 0;
     IConnext public connext; 
     uint32 public domainId;
@@ -22,6 +23,7 @@ contract ETHIndia22NFTSale is ERC721 {
     error IncorrectBuyAmount();
     error TransfersNotPossible();
     error NotAOwner();
+    error AllSold();
     constructor(IConnext _connext, address _target, uint32 _domainId) ERC721("EthIndia22", "ETHIN22") {
         connext = _connext;
         target = _target;
@@ -29,6 +31,7 @@ contract ETHIndia22NFTSale is ERC721 {
     }
 
     function buy() external payable {
+        if(counter == totalSupply - 1) revert AllSold();
         if (msg.value != mintPrice) revert IncorrectBuyAmount();
         _safeMint(msg.sender, ++counter);
     }
@@ -45,18 +48,19 @@ contract ETHIndia22NFTSale is ERC721 {
     }
 
     function propogateToMainnet (
-    uint256 tokenId,
-    uint256 relayerFee
+        uint256 tokenId,
+        uint256 relayerFee
     ) external payable {
-    connext.xcall{value: relayerFee}(
-      domainId, // _destination: Domain ID of the destination chain
-      target,            // _to: address of the target contract
-      address(0),        // _asset: use address zero for 0-value transfers
-      ownerOf(tokenId),        // _delegate: address that can revert or forceLocal on destination
-      0,                 // _amount: 0 because no funds are being transferred
-      0,                 // _slippage: can be anything between 0-10000 because no funds are being transferred
-      abi.encode(tokenId)           // _callData: the encoded calldata to send
-    );
-    _burn(tokenId);
+
+        connext.xcall{value: relayerFee}(
+        domainId, // _destination: Domain ID of the destination chain
+        target,            // _to: address of the target contract
+        address(0),        // _asset: use address zero for 0-value transfers
+        ownerOf(tokenId),        // _delegate: address that can revert or forceLocal on destination
+        0,                 // _amount: 0 because no funds are being transferred
+        0,                 // _slippage: can be anything between 0-10000 because no funds are being transferred
+        abi.encode(tokenId)           // _callData: the encoded calldata to send
+        );
+        _burn(tokenId);
   }
 }
